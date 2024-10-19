8a3886c545505dc58b7d8f7c8a92856a3d4374a1
badAl <badalsingh120403@gmail.com>
2024-10-19 22:52:55 +0530

    feat: set up mysql database connecion
    - install mysql2
    - create dv.ts to initialize db connection

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import dotenv from "dotenv";
+import app from "./app";
+import { createPool } from "./config/db";
 
 dotenv.config();
 
-import app from "./app";
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running at port: ${PORT}`);
+
+  try {
+    await createPool();
+  } catch (error) {
+    console.error("Database connection failed: ", error);
+    process.exit(1);
+  }
 });
